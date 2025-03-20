bc8b2154e878e209972a9d26e5db2b236fc348d7
mathew rym <rymmathew@gmail.com>
2025-03-20 21:21:06 +0300

    Handling working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.getElementById("create-task-form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  
+  const taskText = document.getElementById("new-task-description").value;
+  if (!taskText.trim()) return;
+
+  addTask(taskText);
+  event.target.reset();
 });
+
+
+function addTask(taskText) {
+  const task = document.createElement("li");
+  task.textContent = taskText + " ";
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "X";
+  deleteButton.addEventListener("click", () => deleteTask(task));
+
+  task.appendChild(deleteButton);
+  document.getElementById("tasks").appendChild(task);
+}
+
+function deleteTask(task) {
+  task.remove();
+}
